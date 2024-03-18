1423c45093a850719e09c97cd529009b512805c6
EngineersBox <toastcraft.info@gmail.com>
2024-03-18 16:31:55 +1100

    Added OpenTelemetry instrumentation and collector container

--- start_cassandra.sh
+++ start_cassandra.sh
@@ -8,6 +8,10 @@ GITHUB_USERNAME="${GITHUB_USERNAME:-"EngineersBox"}"
 REPOSITORY="${REPOSITORY:-"cassandra"}"
 BRANCH="${BRANCH:-"cassandra-5.0-beta1"}"
 CASSANDRA_VERSION="${CASSANDRA_VERSION:-"5.0-beta1"}"
+OTEL_JAR_VERSION="${OTEL_JAR_VERSION:-"v2.2.0"}"
+OTEL_JAR_PATH="${OTEL_JAR_PATH:-"/var/lib/otel/opentelemetry-javaagent.jar"}"
+OTEL_AGENT_CONFIG_FILE="${OTEL_AGENT_CONFIG_FILE:-"/etc/otel/otel.properties"}"
+OTEL_SERVICE_NAME="${OTEL_SERVICE_NAME:-"OTEL-Cassandra"}"
 PWD=$(pwd)
 
 cat <<EOF
@@ -20,6 +24,11 @@ REPOSITORY = $REPOSITORY
 BRANCH = $BRANCH
 CASSANDRA_VERSION = $CASSANDRA_VERSION
 
+OTEL_JAR_VERSION = $OTEL_JAR_VERSION
+OTEL_JAR_PATH = $OTEL_JAR_PATH
+OTEL_AGENT_CONFIG_FILE = $OTEL_AGENT_CONFIG_FILE
+OTEL_SERVICE_NAME = $OTEL_SERVICE_NAME
+
 EOF
 
 function unpack_tar() {
@@ -28,6 +37,7 @@ function unpack_tar() {
     echo "$(cat "apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz.sha256") apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz" > "apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz.sha256"
     sha256sum --check --status "apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz.sha256"
     tar xzf "apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz"
+    mv "apache-cassandra-$CASSANDRA_VERSION" "$REPOSITORY"
 }
 
 function build_repo() {
@@ -50,19 +60,42 @@ function build_repo() {
 }
 
 function start_docker() {
-    # TODO: Fix user permissions, at the moment "cassandra" acts like root
+        #-p 4317:4317 \
+        #-p 4318:4318 \
     docker run \
-        -p 7000:7000 \
-        -p 7001:7001 \
-        -p 7199:7199 \
-        -p 9042:9042 \
-        -p 9160:9160 \
-        -v "$PWD/apache-cassandra-$CASSANDRA_VERSION/conf:/etc/cassandra" \
+        --network=host \
+        -v "$PWD/log:/var/log/otel" \
+        -v "$PWD/otel-collector-config.yaml:/etc/otelcol/config.yaml" \
+        --name "otel" \
+        -d \
+        otel/opentelemetry-collector-contrib:0.96.0
+    # TODO: Fix user permissions, at the moment "cassandra" acts like root
+        #-p 7000:7000 \
+        #-p 7001:7001 \
+        #-p 7199:7199 \
+        #-p 9042:9042 \
+        #-p 9160:9160 \
+    docker run \
+        --network=host \
+        -v "$PWD/otel.properties:$OTEL_AGENT_CONFIG_FILE" \
+        -v "$PWD/opentelemetry-javaagent.jar:$OTEL_JAR_PATH" \
+        -v "$PWD/$REPOSITORY/conf:/etc/cassandra" \
         -v "$PWD/cassandra.yaml:/etc/cassandra/cassandra.yaml" \
-        -v "$PWD/apache-cassandra-$CASSANDRA_VERSION:/var/lib/cassandra" \
+        -v "$PWD/$REPOSITORY:/var/lib/cassandra" \
         --name "cassandra" \
         -d \
         $IMAGE
+}
+
+jvm_server_opts=(jvm-server.options jvm11-server.options jvm17-server.options)
+
+function instrument_cassandra() {
+    wget "https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/$OTEL_JAR_VERSION/opentelemetry-javaagent.jar"
+    for opt in ${jvm_server_opts[@]}; do
+        echo "-javaagent:$OTEL_JAR_PATH" >> "$PWD/$REPOSITORY/conf/$opt"
+        echo "-Dotel.javaagent.configuration-file=$OTEL_AGENT_CONFIG_FILE" >> "$PWD/$REPOSITORY/conf/$opt"
+        echo "-Dotel.service.name=$OTEL_SERVICE_NAME" >> "$PWD/$REPOSITORY/conf/$opt"
+    done
 }
 
 case $DISTRIBUTION in
@@ -71,5 +104,6 @@ case $DISTRIBUTION in
     *) echo "[ERROR] Invalid distribution option: $DISTRIBUTION, must be one of [tar,repo]"; exit 1 ;;
 esac
 
+instrument_cassandra
 start_docker
 
