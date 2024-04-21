64a0b4d449f4ea8929a58f6f838062af0e87a2f4
EngineersBox <toastcraft.info@gmail.com>
2024-04-21 15:12:57 +1000

    Added iptables script

---
+++ scripts/apply_iptables_rules.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Exit on any error from child commands
+set -e
+
+SOURCE_IP="$1"
+SOURCE_IP_RANGE="$2"
+SOURCE_PORT="$3"
+
+USE_PERSISTENT_RULES=true
+if ! command -v netfilter-persistent &> /dev/null; then
+    echo "[IPTABLES] ERROR: iptables entries are emphemeral, 'netfilter-persistent' is used to ensure persistence, install the 'iptables-persistent' package to save rule changes"
+    USE_PERSISTENT_RULES=false
+fi
+
+# See: https://www.digitalocean.com/community/tutorials/iptables-essentials-common-firewall-rules-and-commands
+iptables -A INPUT -p tcp -s "$SOURCE_IP/$SOURCE_IP_RANGE" --dport "$SOURCE_PORT" -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+echo "[IPTABLES] Created TCP input allowlist entry for $SOURCE_IP/$SOURCE_IP_RANGE:$SOURCE_PORT"
+iptables -A OUTPUT -p tcp --sport "$SOURCE_PORT" -m conntrack --ctstate ESTABLISHED -j ACCEPT
+echo "[IPTABLES] Created TCP output allowlist entry for port $SOURCE_PORT"
+
+if $USE_PERSISTENT_RULES; then
+    netfilter-persistent save
+    echo "[IPTABLES] Saved rule changes"
+fi
--- scripts/build_cassandra.sh
+++ scripts/build_cassandra.sh
@@ -9,7 +9,9 @@ PWD=$(pwd)
 cd "$REPOSITORY"
 git checkout "$BRANCH"
 git pull
+
 ant artifacts
+
 build_version=$(xmllint -xpath 'string(/project/property[@name="base.version"/@value]) build.xml')
 mv "build/apache-cassandra-$build_version-SNAPSHOT-bin.tar.gz" "$PWD/apache-cassandra-$CASSANDRA_VERSION-bin.tar.gz"
 cd "$PWD"
