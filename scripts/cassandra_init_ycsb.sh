2896ddf5cde047494855dd7c425b64f35966e885
EngineersBox <toastcraft.info@gmail.com>
2024-07-01 12:39:24 +1000

    Added YCSB keyspace/table init script

---
+++ scripts/cassandra_init_ycsb.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+read -r -d '' CQL_SCRIPT << EOM
+create keyspace ycsb with replication = {'class' : 'SimpleStrategy', 'replication_factor': 1 };
+create table ycsb.usertable (
+    y_id varchar primary key,
+    field0 varchar,
+    field1 varchar,
+    field2 varchar,
+    field3 varchar,
+    field4 varchar,
+    field5 varchar,
+    field6 varchar,
+    field7 varchar,
+    field8 varchar,
+    field9 varchar
+) with compaction = {'class': 'LeveledCompactionStrategy'}
+and memtable = 'trie';
+EOM
+
+docker exec cassandra /var/lib/cassandra/bin/cqlsh -e "$CQL_SCRIPT"
