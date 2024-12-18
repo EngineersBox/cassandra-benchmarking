eee51ddc56205b0007c5cab0fa271c8858836f12
EngineersBox <toastcraft.info@gmail.com>
2024-12-18 15:14:26 +1100

    Added bootstrap unit, refactored YCSB base profile into GENI exec instead of bash content

--- provisioner/application/variant/otel_collector.py
+++ provisioner/application/variant/otel_collector.py
@@ -1,8 +1,9 @@
-from provisioner.application.app import AbstractApplication, ApplicationVariant
+from provisioner.application.app import AbstractApplication, ApplicationVariant, LOCAL_PATH
 from provisioner.structure.cluster import Cluster
 from provisioner.structure.node import Node
 from provisioner.provisoner import TopologyProperties
 import geni.portal as portal
+import geni.rspec.pg as pg
 
 class OTELCollector(AbstractApplication):
     ycsb_version: str
@@ -27,6 +28,40 @@ class OTELCollector(AbstractApplication):
         self.ycsb_version = params.ycsb_version
         self.cluster_application = params.application
 
+    def createYCSBBaseProfile(self, node: Node) -> None:
+        base_profile_path=f"{LOCAL_PATH}/ycsb-{self.ycsb_version}/base_profile.dat"
+        app_variant: ApplicationVariant = ApplicationVariant(ApplicationVariant._member_map_[str(self.cluster_application).upper()])
+        all_ips: list[str] = []
+        for iface in self.topologyProperties.nodeInterfaces:
+            all_ips.append(iface.addresses[0].address)
+        profile_content: str = ""
+        if app_variant == ApplicationVariant.CASSANDRA:
+            profile_content = f"""
+            hosts={",".join(all_ips)}
+            port=9042
+            """
+        elif app_variant == ApplicationVariant.ELASTICSEARCH:
+            # TODO: Complete this
+            profile_content = ""
+        elif app_variant == ApplicationVariant.MONGO_DB:
+            # TODO: Complete this
+            profile_content = ""
+        elif app_variant == ApplicationVariant.SCYLLA:
+            profile_content = f"""
+            scylla.hosts={",".join(all_ips)}
+            port=9042
+            """
+        else:
+            raise RuntimeError(f"Invalid application variant: {self.cluster_application}")
+        command = f"""cat <<-EOF {base_profile_path}
+        {profile_content}
+        EOF
+        """
+        node.instance.addService(pg.Execute(
+            shell="bash",
+            command=command 
+        ))
+
     def nodeInstallApplication(self, node: Node) -> None:
         super().nodeInstallApplication(node)
         self.unpackTar(
@@ -37,15 +72,8 @@ class OTELCollector(AbstractApplication):
             node,
             f"https://github.com/brianfrankcooper/YCSB/releases/download/{self.ycsb_version}/ycsb-{self.ycsb_version}.tar.gz"
         )
-        all_ips: list[str] = []
-        for iface in self.topologyProperties.nodeInterfaces:
-            all_ips.append(iface.addresses[0].address)
-        all_ips_prop: str = " ".join([f"\"{address}\"" for address in all_ips]) 
         self.bootstrapNode(
             node,
-            {
-                "YCSB_VERSION": self.ycsb_version,
-                "CLUSTER_APPLICATION": self.cluster_application,
-                "NODE_ALL_IPS": f"({all_ips_prop})"
-            },
+            {},
         )
+        self.createYCSBBaseProfile(node)
