050c8eab9d071df95183d2a36366a34ac4e5c263
EngineersBox <toastcraft.info@gmail.com>
2025-01-08 12:30:29 +1100

    Migrated provisioner profile to separate repo

--- provisioner/structure/rack.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from .node import Node
-
-@dataclass
-class Rack:
-    name: str
-    nodes: list[Node]
--- provisioner/topology.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass, field
-import geni.rspec.pg as pg
-
-@dataclass
-class TopologyProperties:
-    collectorInterface: pg.Interface
-    nodeInterfaces: list[pg.Interface] = field(default_factory=list)
