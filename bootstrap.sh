9b75132f191057b651d233e9d7319bcfa3b986b9
EngineersBox <toastcraft.info@gmail.com>
2025-01-15 15:43:23 +1100

    Updated permissions

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,18 +2,17 @@
 
 set -o errexit -o pipefail -o noclobber -o nounset
 
-source /var/lib/node_env
+source /var/lib/cluster/node_env
 
 echo "[INFO] Starting $APPLICATION_VARIANT services"
-docker compose up -d /var/lib/docker/$APPLICATION_VARIANT/docker-compose.yaml
-
+docker compose up -d /var/lib/cluster/docker/$APPLICATION_VARIANT/docker-compose.yaml
 if [ "x$INVOKE_INIT" != "x"]; then
     python3 -m venv venv
     python3 -m pip install -r requirements.txt
     set > /tmp/$$_env
     case "$APPLICATION_VARIANT" in
         cassandra)
-            python3 init/cassandra.py --environment=/tmp/$$_env
+            python3 /var/lib/cluster/init/cassandra.py --environment=/tmp/$$_env
             ;;
         *)
             ;;
