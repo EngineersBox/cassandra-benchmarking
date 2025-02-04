6d870b5100d389ddf003537cd4187e72e256aa3c
EngineersBox <toastcraft.info@gmail.com>
2025-02-04 13:41:08 +1100

    Added OTEL script to wait for DB cluster to start

--- init/otel.py
+++ init/otel.py
@@ -1,52 +1,52 @@
 from enum import Enum
-import os, time, random, logging
-from cassandra.cluster import Cluster, ResultSet
+import os, time, logging
+from typing import Callable, Optional
+from cassandra.cluster import Cluster, Host
 
 logging.basicConfig(format="[%(levelname)s] %(name)s :: %(message)s", level=logging.DEBUG)
 
-class ApplicationVariant(Enum):
-    CASSANDRA = "cassandra", True
-    MONGO_DB = "mongodb", True
-    SCYLLA = "scylla", True
-    ELASTICSEARCH = "elasticsearch", True,
-    OTEL_COLLECTOR = "otel_collector", False
 
 CONNECT_RETRY_DELAY_SECONDS = 5
 
+def filterUpHosts(hosts: list[Host]) -> list[Host]:
+    return list(filter(lambda host: host.is_up, hosts))
+
+def checkHosts(node_ips: list[str], contact_points: list[str]) -> bool:
+    cluster: Optional[Cluster] = None
+    try:
+        logging.info("Connecting to Cassandra cluster")
+        # Get a connection
+        cluster = Cluster(contact_points=contact_points)
+        cluster.connect()
+        logging.info("Established connection to Cassandra cluster")
+    except:
+        logging.warning("Connection failed, retrying")
+        if (cluster != None):
+            cluster.shutdown()
+        return False
+    if cluster.metadata == None:
+        cluster.shutdown()
+        return False;
+    # Retrieve all cluster nodes (hosts)
+    hosts = cluster.metadata.all_hosts()
+    if len(hosts) != node_ips:
+        logging.info(f"Retrieved {len(hosts)} hosts, but have {len(node_ips)} IPs, retrying")
+        cluster.shutdown()
+        return False
+    # Wait for all nodes to come up
+    up_hosts = filterUpHosts(hosts)
+    if (len(up_hosts) == len(node_ips)):
+        cluster.shutdown()
+        return True
+    logging.info(f"{len(up_hosts)}/{len(node_ips)} nodes are up")
+    cluster.shutdown()
+    return False
+
 def mainCassandra() -> None:
     node_ips = os.environ["CASSANDRA_NODE_IPS"].split(",")
-    # Pop the node because once we connect to the cluster, everything is
-    # ok on that node (in terms of conditions for the rest of OTEL init)
-    # and also because the system.peers_v2 table does not contain the
-    # current node you are connected to.
-    target_node = node_ips.pop(0)
-    session = None
-    # Get a connection
-    while (True):
-        try:
-            logging.info("Connecting to Cassandra cluster")
-            cluster = Cluster(contact_points=[f"{target_node}:9042"])
-            session = cluster.connect()
-            break
-        except:
-            logging.warning("Attempt failed, sleeping for %d seconds", CONNECT_RETRY_DELAY_SECONDS)
-            time.sleep(CONNECT_RETRY_DELAY_SECONDS)
-    
-    logging.info("Established connection to Cassandra cluster")
-    # Wait for all nodes to come up
-    while (len(node_ips) == 0):
-        # Query all peers and their state
-        result_set: ResultSet = session.execute("select peer from system.peers_v2;")
-        rows = result_set.all()
-        # Remove the peers that are up from the list of node_ips we have
-        for row in rows:
-            # FIXME: The system.peers_v2 table doesn't have the state of the
-            #        nodes. Maybe it is easier to just connect to each node,
-            #        pull the system.local table and look at the boostrapped
-            #        column to see if it is 'COMPLETED' to determine if the
-            #        node is up or not.
-            if (row["peer"] in node_ips):
-                node_ips.remove(row["peer"])
+    contact_points = node_ips[:min(len(node_ips), 3)]
+    while (not checkHosts(node_ips, contact_points)):
+        time.sleep(CONNECT_RETRY_DELAY_SECONDS)
     # Sleep for a minute for good measure
     time.sleep(60)
     pass
@@ -60,21 +60,30 @@ def mainScylla() -> None:
 def mainElasticsearch() -> None:
     pass
 
+class ApplicationVariant(Enum):
+    CASSANDRA = mainCassandra
+    MONGO_DB = mainMongoDB
+    SCYLLA = mainScylla
+    ELASTICSEARCH = mainElasticsearch
+    OTEL_COLLECTOR = None
+
+    def isProvisionable(self) -> bool:
+        return self.value != None
+
+    def mainMethod(self) -> Callable[[], None]:
+        if (self.value == None):
+            raise ValueError(f"Variant is non-provisionable: {self.name}")
+        return self.value
+
 def main() -> None:
     env_cluster_application_variant = os.environ["CLUSTER_APPLICATION_VARIANT"].upper()
     if (env_cluster_application_variant not in ApplicationVariant._member_names_):
         raise RuntimeError(f"Unknown variant specified in CLUSTER_APPLICATION_VARIANT env var: {env_cluster_application_variant}")
     app_variant: ApplicationVariant = ApplicationVariant(ApplicationVariant._member_map_[env_cluster_application_variant])
-    if (app_variant == ApplicationVariant.CASSANDRA):
-        mainCassandra()
-    elif (app_variant == ApplicationVariant.MONGO_DB):
-        mainMongoDB()
-    elif (app_variant == ApplicationVariant.SCYLLA):
-        mainScylla()
-    elif (app_variant == ApplicationVariant.ELASTICSEARCH):
-        mainElasticsearch()
-    elif (app_variant == ApplicationVariant.OTEL_COLLECTOR):
-        raise RuntimeError("CLUSTER_APPLICATION_VARIANT cannot be OTEL_COLLECTOR")
+    if (not app_variant.isProvisionable()):
+        raise ValueError(f"CLUSTER_APPLICATION_VARIANT is {env_cluster_application_variant} which is not provisionable and thus cannot be interfaced with")
+    callable: Callable[[], None] = app_variant.mainMethod()
+    callable()
 
 if __name__ == "__main__":
     main()
