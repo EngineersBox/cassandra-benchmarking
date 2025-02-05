2f4a1440150b5ad21f2febb5d51dcb247b7c5771
EngineersBox <toastcraft.info@gmail.com>
2025-02-05 14:52:21 +1100

    Made bootstrapping more efficient with defined stages

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,43 +4,8 @@ set -o errexit -o pipefail -o noclobber
 
 source /var/lib/cluster/node_env
 
-function validate_service_start_timing {
-    case "$SERVICE_START_TIMING" in
-        BEFORE_INIT | AFTER_INIT)
-            ;;
-        *)
-            echo "[ERROR] Invalid value for SERVICE_START_TIMING, must be one of [BEFORE_INIT, AFTER_INIT]"
-            exit 1
-            ;;
-    esac
-}
-
-function start_services() {
-    echo "[INFO] Starting $APPLICATION_VARIANT services"
-    docker compose -f /var/lib/cluster/docker/$APPLICATION_VARIANT/docker-compose.yaml up -d
-}
-
-function node_init() {
-    if [[ -z "$INVOKE_INIT" || "$INVOKE_INIT" == "false" ]]; then
-        echo "[INFO] No init invocation required"
-        return
-    fi
-
-    echo "[INFO] Updating and installing dependencies"
-    sudo apt-get update -y
-    sudo apt-get install -y python3.10-venv
-    python3 -m venv venv
-    source venv/bin/activate
-    python3 -m pip install -r requirements.txt
-
+function preInitStage() {
     case "$APPLICATION_VARIANT" in
-        cassandra)
-            echo "[INFO] Invoking cassandra initialisation"
-            set -a
-            source /var/lib/cluster/node_env
-            python3 ./init/cass.py
-            set +a
-            ;;
         otel_collector)
             echo "[INFO] Invoking OTEL collector initialisation"
             set -a
@@ -49,25 +14,60 @@ function node_init() {
             set +a
             ;;
         *)
+            echo "[INFO] No pre-init operations for $APPLICATION_VARIANT, skipping"
             ;;
     esac
 }
 
+function startServicesStage() {
+    echo "[INFO] Starting $APPLICATION_VARIANT services"
+    docker compose -f /var/lib/cluster/docker/$APPLICATION_VARIANT/docker-compose.yaml up -d
+}
+
+function postInitStage() {
+    case "$APPLICATION_VARIANT" in
+        cassandra)
+            # Only run once on node marked to handle it
+            if [[ -z "$INVOKE_INIT" || "$INVOKE_INIT" != "true" ]]; then
+                return
+            fi
+            echo "[INFO] Invoking cassandra initialisation"
+            set -a
+            source /var/lib/cluster/node_env
+            python3 ./init/cass.py
+            set +a
+            ;;
+        *)
+            echo "[INFO] No post-init operations for $APPLICATION_VARIANT, skipping"
+            ;;
+    esac
+}
+
+declare -A BOOSTRAP_STAGES
+BOOTSTRAP_STAGES["PRE_INIT"]=preInitStage
+BOOTSTRAP_STAGES["START_SERVICES"]=startServicesStage
+BOOTSTRAP_STAGES["POST_INIT"]=postInitStage
+
+function performStages() {
+    for stage in "${!BOOTSTRAP_STAGES[@]}"; do
+        echo "[INFO] Running stage: ${stage}"
+        ${BOOTSTRAP_STAGES[$stage]}
+    done
+}
+
 # === MAIN === #
 
-validate_service_start_timing
 
 pushd /var/lib/cluster
 
-if [[ "$SERVICE_START_TIMING" == "BEFORE_INIT" ]]; then
-    start_services
-fi
+echo "[INFO] Updating and installing dependencies"
+sudo apt-get update -y
+sudo apt-get install -y python3.10-venv
+python3 -m venv venv
+source venv/bin/activate
+python3 -m pip install -r requirements.txt
 
-node_init
-
-if [[ "$SERVICE_START_TIMING" == "AFTER_INIT" ]]; then
-    start_services
-fi
+performStages
 
 popd
 echo "[INFO] Node bootstrapping suceeded"
